da2b6f3144e0eee099f6b7484fe7d6cae61d8fac
Thomas Bnse <pro.bonanseathomas@gmail.com>
2025-01-13 10:48:58 +0100

    Finish welcome page

--- welcomePage.js
+++ welcomePage.js
@@ -2,22 +2,52 @@ const playButton = document.getElementById('playButton')
 const buttonSelected = document.getElementById('selected')
 
 
-const actionButton = () => {
+function actionButton() {
 
-playButton.addEventListener('mouseenter', function () {
 
-    buttonSelected.style.backgroundImage = "url(pictures/TriangleSelector.svg)";
-})
+    playButton.addEventListener('mouseenter', function () {
 
-playButton.addEventListener('mouseleave', function () {
+        buttonSelected.style.backgroundImage = "url(pictures/TriangleSelector.svg)";
+    })
 
-    buttonSelected.style.backgroundImage = "none";
-})
+    playButton.addEventListener('mouseleave', function () {
+
+        buttonSelected.style.backgroundImage = "none";
+    })
 }
 
-playButton.addEventListener('click', function() {
+playButton.addEventListener('click', function () {
     window.location.href = "homePage.html"
 })
 
+function pngSpeaking() {
+
+    let delay = 100;
+    let delay_start = 0;
+    let contents;
+    let letters;
+
+    let elem = document.getElementById('comment')
+
+    if (elem) {
+        contents = elem.textContent.trim();
+        elem.textContent = "";
+        letters = contents.split("");
+        elem.style.visibility = 'visible';
+
+        letters.forEach(function (letter, index_1) {
+            setTimeout(function () {
+                elem.textContent += letter;
+
+            }, delay_start + delay * index_1);
+        });
+        delay_start += delay * letters.length;
+    };
+}
+
+
+
+
+pngSpeaking();
 actionButton();
 
